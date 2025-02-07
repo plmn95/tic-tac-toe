7f81e8e86807f79ad6a634a7da8b8d6d57fa35b6
plmn95 <plmn9595@gmail.com>
2025-02-07 16:11:15 +0200

    fully functional

--- script.js
+++ script.js
@@ -149,13 +149,16 @@ const displayController = (function() {
 
         const container = document.querySelector('.container')
 
+        const divTop = document.createElement('div')
+        divTop.classList.add('divTop')
+        container.appendChild(divTop)
         const btnNew = document.createElement('button')
         btnNew.classList.add('btnNew')
         btnNew.innerText = "NEW GAME"
         btnNew.addEventListener('click', () => {
             gameController.newGame()
         })
-        container.appendChild(btnNew)
+        divTop.appendChild(btnNew)
     
         const player1Div = document.createElement('div')
         player1Div.classList.add('player1Div')
@@ -165,9 +168,16 @@ const displayController = (function() {
         player1Title.innerText = "PLAYER 1"
         player1Div.appendChild(player1Title)
         const score1Text = document.createElement('p')
-        score1Text.classList.add('pScore1')
-        score1Text.innerText = 'SCORE: 0'
+        score1Text.innerText = 'SCORE'
+        score1Text.classList.add('score1Text')
         player1Div.appendChild(score1Text)
+        const pScore1Div = document.createElement('div')
+        pScore1Div.classList.add('pScore1Div')
+        container.appendChild(pScore1Div)
+        const pScore1 = document.createElement('p')
+        pScore1.classList.add('pScore1')
+        pScore1.innerText = '0'
+        pScore1Div.appendChild(pScore1)
     
         const player2Div = document.createElement('div')
         player2Div.classList.add('player2Div')
@@ -177,13 +187,23 @@ const displayController = (function() {
         player2Title.innerText = 'PLAYER 2'
         player2Div.appendChild(player2Title)
         const score2Text = document.createElement('p')
-        score2Text.classList.add('pScore2')
-        score2Text.innerText = 'SCORE: 0'
+        score2Text.innerText = 'SCORE'
+        score2Text.classList.add('score2Text')
         player2Div.appendChild(score2Text)
+        const pScore2Div = document.createElement('div')
+        pScore2Div.classList.add('pScore2Div')
+        container.appendChild(pScore2Div)
+        const pScore2 = document.createElement('p')
+        pScore2.classList.add('pScore2')
+        pScore2.innerText = '0'
+        pScore2Div.appendChild(pScore2)
     
+        const divGameboard = document.createElement('div')
+        divGameboard.classList.add('divGameboard')
+        container.appendChild(divGameboard)
         const gameboardDiv = document.createElement('div')
         gameboardDiv.classList.add('gameboardDiv')
-        container.appendChild(gameboardDiv)
+        divGameboard.appendChild(gameboardDiv)
         gameboard.board.forEach((e, i) => {
             let div = document.createElement('div')
             div.classList.add('boardSpace')
@@ -203,11 +223,11 @@ const displayController = (function() {
         switch (who) {
             case 'player':
                 paragraph = document.querySelector('.pScore1')
-                paragraph.innerText = `SCORE: ${score}`
+                paragraph.innerText = `${score}`
                 break;
             case 'computer':
                 paragraph = document.querySelector('.pScore2')
-                paragraph.innerText = `SCORE: ${score}`
+                paragraph.innerText = `${score}`
                 break;
         }
     }
